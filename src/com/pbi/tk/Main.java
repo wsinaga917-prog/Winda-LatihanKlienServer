e549b786137442a8687a5f2b91d66a19961fd90b
wsinaga917-prog <wsinaga917@gmail.com>
2026-09-25 22:15:13 +0700

    LatihanKlienServer

---
+++ src/com/pbi/tk/Main.java
@@ -0,0 +1,11 @@
+package com.pbi.tk;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Program Klien Server berjalan.");
+        System.out.println("Nama : Winda Sinaga");
+        System.out.println("NIM  : 25110086");
+        System.out.println("Kelas: TK-B Pagi");
+    }
+}
